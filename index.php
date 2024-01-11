9ee2f60d71b26a5593f66786b8f360e3999cf133
jayaprasad356 <jayaprasad356@gmail.com>
2024-01-11 14:45:03 +0530

    index

--- index.php
+++ index.php
@@ -11,6 +11,13 @@ $db->sql("SET NAMES 'utf8'");
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K8BTDKQT');</script>
+<!-- End Google Tag Manager -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
 
     <style>
@@ -28,6 +35,10 @@ $db->sql("SET NAMES 'utf8'");
     </style>
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K8BTDKQT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div class="container mt-5">
         <div class="btn-container">
             <div class="row">
